23e0308e5d8f72bba88a892083ee73e234ebbf6f
Khaia <hkarur@asu.edu>
2022-10-27 22:40:30 -0700

    Four test cases

--- urinals_testing/src/main/java/org/example/Main.java
+++ urinals_testing/src/main/java/org/example/Main.java
@@ -3,34 +3,41 @@ package org.example;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
+//import java.util.regex.Pattern;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 public class Main {
     public static List<String> inputLines=new ArrayList<>();
-    final static Pattern PATTERN = Pattern.compile("(.*?)(?:\\((\\d+)\\))?(\\.[^.]*)?");
+//    final static Pattern PATTERN = Pattern.compile("(.*?)(?:\\((\\d+)\\))?(\\.[^.]*)?");
     public static List<String> result=new ArrayList<>();
     public static void main(String[] args) {
-        openFile();
-        processString();
+        boolean func1=openFile();
+        boolean func2=processString(inputLines);
         boolean func3=writeFile(result);
-
-
-
-        
-    }
-    public static void processString(){
-        int n=inputLines.size();
-        for(int i=0;i<n;i++){
-            Integer answer=countUrinals(inputLines.get(i));
-            result.add(answer.toString());
+        if(func1 && func2 && func3){
+            System.out.println("Result written in file");
         }
 
-
-
-
     }
-    public static void openFile(){
-        String FILE_PATH = "C:\\Users\\Harshi\\Documents\\SER 515\\ICA 8 testing final\\ICA-8-Testing-Harshitha-Karur\\urinals_testing\\src\\main\\java\\org\\example\\";
+    public static boolean processString(List<String> x){
+        x = inputLines;
+        int n= x.size();
+        boolean flag=false;
+        for(int i=0;i<n;i++){
+            Integer answer=countUrinals(x.get(i));
+            result.add(answer.toString());
+            if(i==(n-1)) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+    public static boolean openFile(){
+        boolean flag=false;
+//        String FILE_PATH = "C:\\Users\\Harshi\\Documents\\SER 515\\ICA 8 testing final\\ICA-8-Testing-Harshitha-Karur\\urinals_testing\\src\\main\\java\\org\\example\\";
+//        String FILE_PATH="https://github.com/harshithakarur/ICA-8-Testing-Harshitha-Karur/blob/552eecefa63688e3822e87efd4d0305db2dbb3ff/urinals_testing/src/main/java/org/example/myfile.dat";
+        String FILE_PATH="src/main/java/org/example/";
         BufferedReader br = null;
         try {
             File file = new File(FILE_PATH+"myfile.dat"); // java.io.File
@@ -41,6 +48,7 @@ public class Main {
                 // process the line
                 inputLines.add(line);
             }
+            flag=true;
         }
         catch(IOException e) { e.printStackTrace();}
         finally
@@ -48,6 +56,8 @@ public class Main {
             try { if (br != null) br.close(); }
             catch(IOException e) { e.printStackTrace(); }
         }
+
+        return flag;
 
     }
 
@@ -58,9 +68,9 @@ public class Main {
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
                 FileWriter myWriter = new FileWriter("rule.txt");
-                int n=result.size();
-                for(int i=0;i<n;i++){
-                    String s=result.get(i)+"\n";
+//                int n=result.size();
+                for (String value : result) {
+                    String s = value + "\n";
                     myWriter.write(s);
                 }
                 myWriter.close();
@@ -88,16 +98,16 @@ public class Main {
         int n=s.length();
 
         for(int i=0;i<n;i++){
-            System.out.println(flag);
-            System.out.println(count);
-            if(s.charAt(i)=='1' && flag==true){
+//            System.out.println(flag);
+//            System.out.println(count);
+            if(s.charAt(i)=='1' && flag){
 //                System.out.println("return -1");
                 return -1;
             }
             else if(s.charAt(i)=='1'){
                 flag=true;
             }
-            else if(s.charAt(i)=='0' && flag==false){
+            else if(s.charAt(i)=='0' && !flag){
                 if(i+1<n){
                     if(s.charAt(i+1)=='0'){
                         count++;
@@ -114,8 +124,8 @@ public class Main {
             }
         }
 //        System.out.println("Count:"+count);
-        Integer answer=count;
-        return answer;
+//        Integer answer=count;
+        return count;
 
     }
 }
--- urinals_testing/src/test/java/UrinalsTest.java
+++ urinals_testing/src/test/java/UrinalsTest.java
@@ -11,6 +11,33 @@ public class UrinalsTest {
         System.out.println("======Harshitha Karur======TEST ONE EXECUTED=======");
         List<String> s=new ArrayList<>();
         s.add("0");
-        Assertions.assertEquals( true , Main.writeFile(s));
+        Assertions.assertTrue( Main.writeFile(s));
     }
+
+    @Test
+    void testCountUrinals(){
+        System.out.println("======Harshitha Karur======TEST TWO EXECUTED=======");
+        String s="10001";
+        Assertions.assertEquals( 1 , Main.countUrinals(s));
+    }
+
+    @Test
+    void testProcessString(){
+        System.out.println("======Harshitha Karur======TEST THREE EXECUTED=======");
+//        List<String> s=new ArrayList<>();
+//        s.add("10001");
+//        s.add("1001");
+//        s.add("110");
+//        s.add("1000100");
+        String s="100001";
+        Assertions.assertEquals( 1 , Main.countUrinals(s));
+    }
+
+    @Test
+    void testOpenFile(){
+        System.out.println("======Harshitha Karur======TEST FOUR EXECUTED=======");
+        Assertions.assertTrue( Main.openFile());
+    }
+
+
 }
